5350ccf3115c87114720a5f18fb0ab02c76f98fb
Isaiah Smith <isaiahsmith1776@gmail.com>
2025-08-27 13:15:41 -0400

    a;sldkfj

--- backend.py
+++ backend.py
@@ -1,1 +1,10 @@
-print("Hello Git! (this should be on git)")
+from flask import Flask, render_template
+
+app = Flask(__name__)  # Initialize Flask app
+
+@app.route("/")  # Home page route
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)  # Debug mode for development
