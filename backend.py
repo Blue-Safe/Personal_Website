3b14d65bc818170d000c2445937c0243d2a7dafc
Isaiah Smith <isaiahsmith1776@gmail.com>
2025-08-31 16:43:31 -0400

    Edited temp icon

--- backend.py
+++ backend.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)  # Initialize Flask app
+app = Flask(__name__)
 
-@app.route("/")  # Home page route
+@app.route("/")
 def home():
     return render_template("home.html")
-
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
 if __name__ == "__main__":
-    app.run(debug=True)  # Debug mode for development
+    app.run(debug=True)
