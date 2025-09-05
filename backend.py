499350d52567975a39ac9109ca42d47b7502bd02
Isaiah Smith <isaiahsmith1776@gmail.com>
2025-09-05 12:15:03 -0400

    Refined blog section, adding captions, proper img spacing, and made progress on the main blog page, adding a "milestone" section and another key pointing towards a boolean in the post dictionary, "highlight".

--- backend.py
+++ backend.py
@@ -7,16 +7,18 @@ posts = [
     {
       "slug": "mark I recap",
       "title": "CRUNCH Mark I -Recap", 
-      "date": "2025-09-02", 
+      "date": "2025-09-02",
+      "highlight": True, 
       "summary":"...",
-      "body": "<p>All right, after taking a break, I'm ready to comeback with full force. I have 2 and a half weeks before I go back to school. So, here are my thoughts on MK I.</p> <h3> What went well </h3> <li> <h4> 3D printed frame</h4> <p>-Was strong, solid, and straightforward to assemble.</p></li> <li> <h4> On board electronics </h4> <p>-Everything was compatable & worked. PCA board was a great call. I succsesfully could program all the moving parts and have them work together.</p></li>",
-      "imgs": ["""<img src="{{ url_for('static', filename='images/MK1_coverON.jpg') }}">""",
-               """<img src="{{ url_for('static', filename='images/MK1_skelly.jpg') }}">"""]
+      "body": "<p>All right, after taking a break, I'm ready to comeback with full force. I have 2 and a half weeks before I go back to school. So, here are my thoughts on MK I.</p> <h3> What went well </h3> <li> 3D printed frame <p>-Was strong, solid, and straightforward to assemble.</p></li> <li>  On board electronics <p>-Everything was compatable & worked. PCA board was a great call. I succsesfully could program all the moving parts and have them work together.</p></li>",
+      "imgs": [("images/MK1_coverON.jpg","Testing"),
+               ("images/MK1_skelly.jpg","This is also a test")]
       },
       {
           "slug": "bluetooth gui",
           "title": "bluetooth gui",
           "date": "2025-08-02",
+          "highlight": False,
           "summary": "Made a GUI in python that communicated with the terminal to send signals over bluetooth",
           "body": "<p>Details...</p>"
       }
